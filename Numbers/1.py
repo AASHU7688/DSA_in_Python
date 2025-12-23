7f2dace31eb50853efee3126cb9c72a203ad28c6
Aashutosh Tiwari <158829026+AASHU7688@users.noreply.github.com>
2025-12-23 21:56:47 +0530

    Check if a number is palindrome or not

--- Numbers/1.py
+++ Numbers/1.py
@@ -3,16 +3,15 @@ n=int(input("enter the number: "))
 
 original = n
 rev = 0
-
 digits=len(str(n))
 
 for i in range(digits):
-    digit=n%10
-    rev=rev*10+digit
+    digits=n%10
+    rev = rev * 10 + digits
     n=n//10
     
 if rev == original:
     print("Number is palindrome")
-    
+
 else:
-    print("Number is not an palindrome")
+    print("Number is not palindrome")
